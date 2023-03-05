bf20aecfc1c0141c3b27eea61513691f1e9027fc
Expector <expector-hutch@outlook.com>
2023-03-05 21:56:18 +0800

    up readme

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# e-editor
-A code editor
+# tpip
--- _widgets/_command.py
+++ _widgets/_command.py
@@ -1,11 +1,12 @@
 from textual.widget import *
 from textual.widgets import Input
 
-import os
+from pip._internal.cli.main import main as pip_run
+
 
 class Command(Input):
     async def action_submit(self) -> None:
         """Handle a submit action (normally the user hitting Enter in the input)."""
         await self.post_message(self.Submitted(self, self.value))
-        os.system(self.value)
+        pip_run(self.value.split())
         self.value = ''
