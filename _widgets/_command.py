66fc48e0dbc78dcffd1abbdb0bb37701900318ba
Expector <expector-hutch@outlook.com>
2023-03-05 21:38:19 +0800

    up cmd

--- _widgets/_command.py
+++ _widgets/_command.py
@@ -1,5 +1,11 @@
 from textual.widget import *
 from textual.widgets import Input
 
+import os
+
 class Command(Input):
-    ...
+    async def action_submit(self) -> None:
+        """Handle a submit action (normally the user hitting Enter in the input)."""
+        await self.post_message(self.Submitted(self, self.value))
+        os.system(self.value)
+        self.value = ''
